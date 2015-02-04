09a726c91f23d4f0b4452853440255f0b65a5f39
Troy Manary <troy.manary@gmail.com>
2015-02-04 09:10:59 -0500

    Created the avgWeight class

---
+++ avgWeight.lua
@@ -0,0 +1,34 @@
+local avgWeight = {}
+avgWeight.__index = avgWeight
+
+function avgWeight.new(factor, value)
+  value = value or 0
+  local self = setmetatable({}, avgWeight)
+  self.x = value
+  self.weight = factor
+ return self
+end
+
+function avgWeight.reset(self, value)
+  value = value or 0
+  self.x = value
+end
+
+function avgWeight.update(self, value)
+  self.x = (self.x * (1-self.weight) + value * self.weight)
+  return self.x
+end
+
+function avgWeight.getValue(self)
+  return self.x
+end
+
+function avgWeight.print(self)
+  print(self.x)
+end
+
+function avgWeight.printState(self)
+  print(self.x, self.weight)
+end
+
+return avgWeight
